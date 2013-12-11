8bc35d72bc6412ace2a862aa8e331231cd838073
Ammon Casey <ammon@caseyohana.com>
2013-12-11 09:44:35 -1000

    Fixed syntax error

--- manifests/httpd.pp
+++ manifests/httpd.pp
@@ -12,8 +12,8 @@
 class server::httpd (
     $server_name    = $server::params::server_name,
     $docroot        = $server::params::docroot,
-    $dir_options    = $server::params::dir_options
-    $dir_override   = $server::params::dir_override
+    $dir_options    = $server::params::dir_options,
+    $dir_override   = $server::params::dir_override,
 ) {
     #------------------------------------------
     # Apache
